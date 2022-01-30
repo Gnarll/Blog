2cb70281410cd42b808d9a60c4b304aabdb60b01
Gnarll <vadimfedorovich22@mail.ru>
2022-01-30 19:24:26 +0300

    implement actions with blogposts on server

---
+++ src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "http://7a85-79-170-109-193.ngrok.io", // It must be changed in next session
+});
--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,16 +1,11 @@
 import createDataContext from "./createDataContext";
+import jsonServer from "../api/jsonServer";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
-    case "add_blogpost":
-      return [
-        ...state,
-        {
-          title: action.payload.title,
-          content: action.payload.content,
-          id: Math.floor(Math.random() * 9999999),
-        },
-      ];
+    case "get_blogposts":
+      return action.payload;
+
     case "delete_blogpost":
       return state.filter((item) => action.payload !== item.id);
     case "edit_blogpost":
@@ -22,19 +17,30 @@ const blogReducer = (state, action) => {
   }
 };
 
+const getBlogPosts = (dispatch) => {
+  return async () => {
+    const response = await jsonServer.get("/blogposts");
+    dispatch({ type: "get_blogposts", payload: response.data });
+  };
+};
+
 const addBlogPost = (dispatch) => {
-  return (title, content, callback) => {
-    dispatch({ type: "add_blogpost", payload: { title, content } });
+  return async (title, content, callback) => {
+    await jsonServer.post("blogposts", { title, content });
     if (callback) callback();
   };
 };
 
 const deleteBlogPost = (dispatch) => {
-  return (id) => dispatch({ type: "delete_blogpost", payload: id });
+  return async (id) => {
+    await jsonServer.delete(`blogposts/${id}`);
+    dispatch({ type: "delete_blogpost", payload: id });
+  };
 };
 
 const editBlogPost = (dispatch) => {
-  return (title, content, id, callback) => {
+  return async (title, content, id, callback) => {
+    await jsonServer.put(`blogposts/${id}`, { title, content });
     dispatch({ type: "edit_blogpost", payload: { title, content, id } });
     if (callback) callback();
   };
@@ -42,6 +48,6 @@ const editBlogPost = (dispatch) => {
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost, editBlogPost },
-  [{ title: "TEST POST", content: "SOME CONTENT", id: 1 }]
+  { addBlogPost, deleteBlogPost, editBlogPost, getBlogPosts },
+  []
 );
